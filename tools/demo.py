e7b1bc4d31d08647d64d6f0d3aa1b72c87b4ad2f
Chris Olivier <cjolivier01@apache.org>
2023-12-08 18:54:41 -0800

    well, pillar ok

--- tools/demo.py
+++ tools/demo.py
@@ -24,15 +24,15 @@ from det3d.torchie.apis import (
     train_detector,
 )
 from det3d.torchie.trainer import load_checkpoint
-import pickle 
-import time 
-from matplotlib import pyplot as plt 
+import pickle
+import time
+from matplotlib import pyplot as plt
 from det3d.torchie.parallel import collate, collate_kitti
 from torch.utils.data import DataLoader
 import matplotlib.cm as cm
 import subprocess
 import cv2
-from tools.demo_utils import visual 
+from tools.demo_utils import visual
 from collections import defaultdict
 
 def convert_box(info):
@@ -45,15 +45,15 @@ def convert_box(info):
 
     detection['box3d_lidar'] = boxes
 
-    # dummy value 
-    detection['label_preds'] = np.zeros(len(boxes)) 
+    # dummy value
+    detection['label_preds'] = np.zeros(len(boxes))
     detection['scores'] = np.ones(len(boxes))
 
-    return detection 
+    return detection
 
 def main():
-    cfg = Config.fromfile('configs/nusc/pp/nusc_centerpoint_pp_02voxel_two_pfn_10sweep_demo.py')
-    
+    cfg = Config.fromfile('configs/nusc/pp/hm_nusc_centerpoint_pp_02voxel_two_pfn_10sweep_demo.py')
+
     model = build_detector(cfg.model, train_cfg=None, test_cfg=cfg.test_cfg)
 
     dataset = build_dataset(cfg.data.val)
@@ -63,27 +63,31 @@ def main():
         batch_size=1,
         sampler=None,
         shuffle=False,
-        num_workers=8,
+        #num_workers=8,
+        num_workers=0,
         collate_fn=collate_kitti,
         pin_memory=False,
     )
 
-    checkpoint = load_checkpoint(model, 'work_dirs/centerpoint_pillar_512_demo/latest.pth', map_location="cpu")
+    #checkpoint = load_checkpoint(model, 'work_dirs/centerpoint_pillar_512_demo/latest.pth', map_location="cpu")
+    checkpoint = load_checkpoint(model, 'pretrained/pillar/latest.pth', map_location="cpu")
     model.eval()
 
     model = model.cuda()
 
     cpu_device = torch.device("cpu")
 
-    points_list = [] 
-    gt_annos = [] 
-    detections  = [] 
+    points_list = []
+    gt_annos = []
+    detections  = []
 
     for i, data_batch in enumerate(data_loader):
         info = dataset._nusc_infos[i]
         gt_annos.append(convert_box(info))
-
-        points = data_batch['points'][:, 1:4].cpu().numpy()
+        um_points = data_batch['points']
+        if isinstance(um_points, list):
+            um_points = torch.stack(um_points)
+        points = um_points[:, 1:4].cpu().numpy()
         with torch.no_grad():
             outputs = batch_processor(
                 model, data_batch, train_mode=False, local_rank=0,
@@ -97,13 +101,13 @@ def main():
             detections.append(output)
 
         points_list.append(points.T)
-    
+
     print('Done model inference. Please wait a minute, the matplotlib is a little slow...')
-    
+
     for i in range(len(points_list)):
         visual(points_list[i], gt_annos[i], detections[i], i)
         print("Rendered Image {}".format(i))
-    
+
     image_folder = 'demo'
     video_name = 'video.avi'
 
@@ -113,7 +117,7 @@ def main():
     height, width, layers = frame.shape
 
     video = cv2.VideoWriter(video_name, 0, 1, (width,height))
-    cv2_images = [] 
+    cv2_images = []
 
     for image in images:
         cv2_images.append(cv2.imread(os.path.join(image_folder, image)))
